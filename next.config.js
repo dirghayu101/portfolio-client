91dfaff6cf05706580a46e182771e221e5d6fca9
Dirghayu Joshi <dirghayujoshi48@gmail.com>
2025-02-14 19:32:40 -0500

    Fixed some errors. Light and dark theme still not working. That's the only one currently diagnosed so can't say for sure, more issues to be fixed.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   env: {
 
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
   
 module.exports = nextConfig;
